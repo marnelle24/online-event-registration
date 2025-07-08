d0d76fe1855c954f076760c2017b49f2f7860263
MarnelleApat <marnelle.apat@bible.org.sg>
2025-07-08 16:39:26 +0800

    add registration module index

--- app/Livewire/Registrant/Index.php
+++ app/Livewire/Registrant/Index.php
@@ -3,11 +3,25 @@
 namespace App\Livewire\Registrant;
 
 use Livewire\Component;
+use App\Models\Registrant;
 
 class Index extends Component
 {
+    public $programmeId;
+    public $search;
+
     public function render()
     {
-        return view('livewire.registrant.index');
+        $registrants = Registrant::query()
+            ->where('programme_id', $this->programmeId)
+            ->when($this->search, function ($query) {
+                $query->where('name', 'like', "%{$this->search}%");
+                $query->orWhere('email', 'like', "%{$this->search}%");
+            })
+            ->latest('created_at')
+            ->get();
+        return view('livewire.registrant.index', [
+            'registrants' => $registrants
+        ]);
     }
 }
--- database/migrations/2025_07_08_033226_create_registrants_table.php
+++ database/migrations/2025_07_08_033226_create_registrants_table.php
@@ -13,6 +13,34 @@ return new class extends Migration
     {
         Schema::create('registrants', function (Blueprint $table) {
             $table->id();
+            $table->string('regCode')->unique(); // orderNumber
+            $table->string('programCode');
+            $table->foreignId('programme_id')->constrained('programmes')->onDelete('cascade');
+            $table->string('nric')->nullable();
+            $table->string('title')->nullable();
+            $table->string('firstName')->nullable();
+            $table->string('lastName')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('postalCode')->nullable();
+            $table->string('email')->nullable();
+            $table->string('contactNumber')->nullable();
+            $table->text('extraFields')->nullable();
+            $table->string('paymentStatus')->nullable();
+            $table->decimal('price', 8, 2)->default(0);
+            $table->decimal('discountAmount', 8, 2)->default(0);
+            $table->decimal('netAmount', 8, 2)->default(0);
+            $table->string('paymentOption')->nullable(); // paynow|card
+            $table->string('paymentReferenceNo')->nullable();
+            $table->string('regStatus')->nullable();
+            $table->string('groupRegistrationID')->nullable();
+            $table->unsignedBigInteger('promocode_id')->nullable();
+            $table->unsignedBigInteger('promotion_id')->nullable();
+            $table->boolean('emailStatus')->default(0);
+            $table->string('confirmedBy')->nullable();
+            $table->string('approvedBy')->nullable();
+            $table->date('approvedDate')->nullable();
+            $table->boolean('soft_delete')->default(0);
             $table->timestamps();
         });
     }
